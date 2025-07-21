d103cbf368364d0e73ed174305c38d6319eb7965
Nirmal <nirmalnirmal0123456@gmail.com>
2025-07-21 21:36:59 +0530

    File sys read and write

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const fs = require ("fs");
+
+/*
+fs.writeFile("message.txt","I am here to save ",(err)=>{
+    if (err) throw new err;
+    console.log ("File saved");
+});
+*/
+
+
+fs.readFile("./message.txt","utf-8",(err,data)=>{
+     if (err) throw new err;
+    console.log (data)
+});
+
 console.log("Hi I am Nirmal");
 
 const a = 10;
